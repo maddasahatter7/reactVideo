952dbea4fa40537917ec7f8051af16d4dbf9b70a
Madison Barnett <Madison.barnett7@gmail.com>
2018-03-06 22:04:29 -0500

    src

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Jumbo = () =>
+
+
+
+    <div className="jumbotron text-center jumbotron">
+        <div className="container">
+            <h1> i H e s h </h1>
+            <h2>Will You Accept The Challenge?</h2>
+        </div>
+    </div>
+
+
+export default Jumbo;
---
+++ src/config.js
@@ -0,0 +1,7 @@
+const API = __DEV__ ? 'http://localhost:3000/api/v1/videos' : '/api/v1/videos';
+const API_KEY = 'AYvePQgbcTkyMg8Bv9rzQz';
+
+export {
+  API,
+  API_KEY,
+};
